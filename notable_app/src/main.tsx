6301f32f586f43c723cc5158ddb054176de57749
HungLam2801 <lamxuanhung01282004@gmail.com>
2024-08-26 14:38:50 +0700

    fix_bug

--- notable_app/src/main.tsx
+++ notable_app/src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
+import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -10,5 +10,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// Đăng ký Service Worker
-serviceWorkerRegistration.register();
+
+reportWebVitals(console.log);
