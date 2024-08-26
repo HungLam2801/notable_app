fe77b460310623deeb84bce3b9734ab9dfd18f3d
HungLam2801 <lamxuanhung01282004@gmail.com>
2024-08-26 16:19:46 +0700

    narbav

--- notable_app/src/App.tsx
+++ notable_app/src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-// import './quill-custom.css'; // Import CSS tùy chỉnh
 import './App.css';
 
 const App: React.FC = () => {
--- notable_app/src/main.tsx
+++ notable_app/src/main.tsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import reportWebVitals from './reportWebVitals';
+import PrimarySearchAppBar from './component/Navbar';
 ReactDOM.render(
   <React.StrictMode>
+   <PrimarySearchAppBar />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
